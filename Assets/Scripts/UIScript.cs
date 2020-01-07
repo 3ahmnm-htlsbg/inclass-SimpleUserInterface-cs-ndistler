1936bd44739c39b5662f8693c944484336daa58f
niklasdistler <niklas.distler03@htl-salzburg.ac.at>
2020-01-07 14:28:29 +0100

    made Interface; added Buttons, Text and InputFields; added + modified Script

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text NameText;
+    public Text Counter;
+    public Button PrintName;
+    public Button Count;
+    public InputField Name;
+
+    public int CountNumber = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetName()
+    {
+        NameText.text = Name.text;
+    }
+
+    public void CountUP()
+    {
+        CountNumber++;
+        Counter.text = CountNumber.ToString();
+    }
+}
